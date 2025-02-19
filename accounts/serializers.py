040efa45d2dc938a42c7fa006b7765da9ccebc7a
Davronbek Nazarov <dav2danderson@gmail.com>
2025-02-19 22:10:45 +0500

    Add Authentication

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,13 +1,21 @@
 from rest_framework import serializers
 
-from .models import CustomUser
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CustomUser
-        fields =['id', 'first_name', 'last_name', 'phone_number', 'city', 'birth_date']
+        model = User
+        fields =['id', 'username', 'first_name', 'last_name', 'phone_number', 'city', 'birth_date', 'password']
         extra_kwargs ={'password': {"write_only": True}}
 
     def create(self, validated_data):
-        return CustomUser.objects.create_user(**validated_data)
+        return User.objects.create_user(**validated_data)
+
+
+class CustomUserRetrieveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name', 'phone_number', 'city', 'birth_date']
+
