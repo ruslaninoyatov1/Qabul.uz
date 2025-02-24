5ac74952615f0fd398049417a4ef1f25256b1996
dav2t <dav2danderson@gmail.com>
2025-02-24 21:28:34 +0500

    Model-ga o'zgartirish kiritildi

--- applications/models.py
+++ applications/models.py
@@ -32,6 +32,7 @@ class Application(models.Model):
     uploaded_date = models.DateTimeField(auto_now_add=True, verbose_name="Yuklangan sana")
     status = models.CharField(max_length=100, choices=DOCUMENT_STATUS, verbose_name="Holat")
     reason = models.TextField(verbose_name="Rad etish sababi", null=True, blank=True)
+    time_to_come = models.DateTimeField(null=True, blank=True, verbose_name="Vaqtni belgilash")
 
     def clean(self):
             if self.status == "Rad etildi" and not self.reason:
